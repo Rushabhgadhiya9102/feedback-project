8c7593205ed153539ec3519af890b63aa2578288
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-05-21 00:59:02 +0530

    done

--- src/components/RatingsSection.jsx
+++ src/components/RatingsSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Gameicons1 from "../assets/similar-games-1.webp";
 import Gameicons2 from "../assets/similar-games-2.webp";
 import Gameicons3 from "../assets/similar-games-3.webp";
@@ -6,12 +6,35 @@ import Gameicons4 from "../assets/similar-games-4.webp";
 import Gameicons5 from "../assets/similar-games-5.webp";
 import Gameicons6 from "../assets/similar-games-6.webp";
 import { FaArrowRight, FaStar } from "react-icons/fa6";
+import { LuTrash2 } from "react-icons/lu";
 
 const RatingsSection = () => {
+
+  // ---------- U S E - S T A T E ------------
+
   const [hover, setHover] = useState(0);
   const [star, setStar] = useState(0);
   const [feedback, setFeedBack] = useState({});
   const [list, setList] = useState([]);
+
+  // ----------- U S E - E F F E C T --------------
+  
+  useEffect(() => {
+
+    let savedList = JSON.parse(localStorage.getItem("list")) || [];
+    setList(savedList);
+    console.log("getitem:", savedList);
+
+  }, []);
+
+  useEffect(() => {
+
+    localStorage.setItem("list", JSON.stringify(list));
+    console.log("setitem:", list);
+
+  }, [list]);
+
+  // ---------- H A N D L E R S - H O V E R ------------
 
   const handleHover = (index) => {
     setHover(index);
@@ -21,26 +44,46 @@ const RatingsSection = () => {
     }
   };
 
+  // ---------- H A N D L E R S - L E A V E ------------
+
   const handleLeave = (index) => {
     setHover(0);
     setStar(index);
   };
 
+  // ---------- H A N D L E R S - D O W N ------------
+
   const handleDown = (index) => {
     setStar(index);
   };
 
+  // ---------- H A N D L E R S - C H A N G E ------------
+
   const handleChange = (event) => {
+
     let { name, value } = event.target;
     setFeedBack({ ...feedback, [name]: value });
+
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setList([...list, { ...feedback, star: star }]);
+  // ---------- H A N D L E R S - S U B M I T ------------
 
-    setFeedBack({ message: "" });
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+    setList([...list, { ...feedback, star: star, id: Date.now()}]);
+    setFeedBack({message: ''});
     setStar(0);
+
+  };
+
+  // ---------- H A N D L E R S - D E L E T E ------------
+
+  const handleDelete = (id) => {
+
+    let feedbackData = list.filter((val) => val.id !== id);
+    setList(feedbackData);
+
   };
 
   const GamesData = [
@@ -110,8 +153,8 @@ const RatingsSection = () => {
                     ))}
 
                     <textarea
+                      id="Message"
                       name="message"
-                      id=""
                       className="form-control my-3"
                       placeholder="Provide Feedback"
                       value={feedback.message || ""}
@@ -125,32 +168,35 @@ const RatingsSection = () => {
 
               <ul className="list-unstyled overflow-y-scroll">
                 {list.map((val, index) => (
-                  <>
-                    <li key={index} className="py-3">
-                      {[...Array(5).keys()].map((_, index) => (
-                        <FaStar
-                          size={20}
-                          color={val.star > index ? "green" : "lightgray"}
-                        />
-                      ))}
-                      <p className="mt-3 bg-light p-3 rounded-3">
-                        {val.message}
-                      </p>
-                      <div className="d-flex align-items-center gap-2">
-                        <p>Did you find it helpful?</p>
-                        <button className="btn btn-light rounded-pill">
-                          Yes
-                        </button>
-                        <button className="btn btn-light rounded-pill">
-                          No
-                        </button>
-                      </div>
-                    </li>
+                  <li key={val.id} className="py-3">
+                    {[...Array(5).keys()].map((_, index) => (
+                      <FaStar
+                        key={index}
+                        size={20}
+                        color={val.star > index ? "green" : "lightgray"}
+                      />
+                    ))}
+
+                    <p className="mt-3 bg-light p-3 rounded-3">{val.message}</p>
+                    <div className="d-flex align-items-center gap-2">
+                      <p>Did you find it helpful?</p>
+                      <button className="btn btn-light rounded-pill">
+                        Yes
+                      </button>
+                      <button className="btn btn-light rounded-pill">No</button>
+                      <button
+                        className="btn text-danger"
+                        onClick={() => handleDelete(val.id)}
+                      >
+                        <LuTrash2 size={20} />
+                      </button>
+                    </div>
                     <hr />
-                  </>
+                  </li>
                 ))}
               </ul>
             </div>
+
             <div className="col-lg-4">
               <div className="similar-item">
                 <div className="title">
@@ -160,22 +206,22 @@ const RatingsSection = () => {
                 </div>
                 <div className="content">
                   <ul className="list-unstyled mt-5 h-75">
-                    {GamesData.map((val) => {
+                    {GamesData.map((val, index) => {
                       const { name, company, rating, img } = val;
 
                       return (
-                        <li className="d-flex gap-3">
+                        <li className="d-flex gap-3" key={index}>
                           <img
                             src={img}
                             className="img-fluid rounded-4"
-                            alt=""
+                            alt="icons-img"
                             style={{ width: "70px", height: "70px" }}
                           />
                           <div>
                             <p className="mb-0">{name}</p>
                             <p className="mb-0">{company}</p>
                             <p className="d-flex align-items-center gap-1">
-                             {rating} <FaStar />
+                              {rating} <FaStar />
                             </p>
                           </div>
                         </li>
